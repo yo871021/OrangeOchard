847e88b942dbc8baa19716765c2f11bb196cbb0e
yo871021 <yo871021@gmail.com>
2025-01-19 22:04:02 +0800

    [feat]PrimeNG flex TopbarMenu

--- ng-orange-orchard/src/app/app.component.ts
+++ ng-orange-orchard/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from "./topbar/topbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ ng-orange-orchard/src/app/topbar/topbar.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-topbar',
+  imports: [MenubarModule],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss'
+})
+export class TopbarComponent {
+  items: MenuItem[] | undefined = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home'
+    },
+    {
+      label: 'Features',
+      icon: 'pi pi-star'
+    },
+    {
+      label: 'Projects',
+      icon: 'pi pi-search',
+      items: [
+        {
+          label: 'Components',
+          icon: 'pi pi-bolt'
+        },
+        {
+          label: 'Blocks',
+          icon: 'pi pi-server'
+        },
+        {
+          label: 'UI Kit',
+          icon: 'pi pi-pencil'
+        },
+        {
+          label: 'Templates',
+          icon: 'pi pi-palette',
+          items: [
+            {
+              label: 'Apollo',
+              icon: 'pi pi-palette'
+            },
+            {
+              label: 'Ultima',
+              icon: 'pi pi-palette'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      label: 'Contact',
+      icon: 'pi pi-envelope'
+    }
+  ];
+}
