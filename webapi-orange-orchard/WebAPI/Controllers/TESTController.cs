194d4171c4f41371d8599e758661e8b34979a19a
yo871021 <yo871021@gmail.com>
2025-02-22 23:31:32 +0800

    [feat]WebAPI

---
+++ webapi-orange-orchard/WebAPI/Controllers/TESTController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class TESTController : BaseController
+    {
+        private readonly ILogger<TESTController> _logger;
+
+        public TESTController(ILogger<TESTController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
